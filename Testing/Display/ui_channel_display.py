bf4d9a8e144eed556d8c47e62e35db1afaefdcee
Jacob Allen <jacob@polyomino.xyz>
2018-02-20 13:00:46 +0000

    Continued work on display, still some issues though

--- Testing/Display/ui_channel_display.py
+++ Testing/Display/ui_channel_display.py
@@ -2,23 +2,70 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GObject, Pango
+from gi.repository import Gtk, Gdk, GObject, Pango
 
 class UIChannelDisplay(Gtk.Window):
     channel_window_index = 0
 
-    def __init__(self, parent):
-        Gtk.Window.__init__(self, title="Child Window")
+    def __init__(self, parent, start_slot=1, channel_size=3):
+        Gtk.Window.__init__(self, title="EMPR Display - Channel Viewer")
         self.parent = parent
-        self.set_default_size(500,300)
-        self.set_border_width(30)
-        label = Gtk.Label(label="Label on child window")
-        self.add(label)
+        self.set_default_size(200,200)
+        self.set_border_width(10)
+        self.start_slot = start_slot
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        vbox.set_homogeneous(False)
+
+        label = Gtk.Label(label="Channel Data")
+        vbox.pack_start(label, False, True, 0)
+
+        scrolledwindow = Gtk.ScrolledWindow()
+
+        liststore = Gtk.ListStore(str, str)
+        self.packet_data = liststore
+        for i in range(channel_size):
+            liststore.append([str(i+start_slot), "0x00"])
+        self.update_values()
+
+        treeview = Gtk.TreeView(model=liststore)
+        fontdesc = Pango.FontDescription("monospace 10")
+        treeview.modify_font(fontdesc)
+        renderer_slot = Gtk.CellRendererText()
+        column_slot = Gtk.TreeViewColumn("Slot", renderer_slot, text=0)
+        treeview.append_column(column_slot)
+        renderer_value = Gtk.CellRendererText()
+        column_value = Gtk.TreeViewColumn("Value", renderer_value, text=1)
+        treeview.append_column(column_value)
+
+        scrolledwindow.add(treeview)
+        vbox.pack_start(scrolledwindow, True, True, 0)
+
+        label = Gtk.Label(label="Channel Colour")
+        vbox.pack_start(label, False, True, 0)
+
+        color = Gdk.color_parse("#ff6600")
+        rgba = Gdk.RGBA.from_color(color)
+
+        area = Gtk.DrawingArea()
+        area.set_size_request(24, 24)
+        area.override_background_color(0, rgba)
+
+        vbox.pack_start(area, False, True, 0)
+
+        self.add(vbox)
+
         self.show_all()
-        self.id = self.channel_window_index
-        self.channel_window_index += 1
+
+        self.id = UIChannelDisplay.channel_window_index
+        self.connect("destroy", self.on_close)
+        UIChannelDisplay.channel_window_index += 1
+
+    def update_values(self):
+        index = self.start_slot - 1
+        for value in self.parent.packet_last_value[:]:
+            self.packet_data[index] = [str(index + self.start_slot), hex(value).ljust(4, "0")]
 
     def on_close(self, event):
-        self.parent.channel_windows.remove(self)
-        print(self.parent.channel_windows)
+        del self.parent.channel_windows[self.id];
         self.destroy()
--- Testing/Display/user_interface.py
+++ Testing/Display/user_interface.py
@@ -16,7 +16,8 @@ class DisplayUI(Gtk.Window):
         self.mode = "SINGLE_CAPTURE"
         Gtk.Window.__init__(self, title="EMPR PC Display")
         self.set_default_size(800, 600)
-        self.channel_windows = []
+        self.set_border_width(10)
+        self.channel_windows = {}
 
         hbox = Gtk.Box(spacing=10)
         hbox.set_homogeneous(False)
@@ -30,39 +31,71 @@ class DisplayUI(Gtk.Window):
 
         label = Gtk.Label("Packet Count: 0")
         label.set_line_wrap(True)
-        vbox_right.pack_start(label, True, True, 0)
+        vbox_right.pack_start(label, False, True, 0)
         self.packet_count_label = label
 
-        text_data = Gtk.TextView()
-        text_data.set_editable(False)
-        text_data.set_wrap_mode(3)
+        scrolledwindow = Gtk.ScrolledWindow()
+
+        liststore = Gtk.ListStore(str, str)
+        self.packet_data = liststore
+        for i in range(512):
+            liststore.append([str(i+1), "0x00"])
+
+        treeview = Gtk.TreeView(model=liststore)
         fontdesc = Pango.FontDescription("monospace 10")
-        text_data.modify_font(fontdesc)
-        vbox_right.pack_start(text_data, True, True, 0)
-        self.packet_data = text_data
+        treeview.modify_font(fontdesc)
+        renderer_slot = Gtk.CellRendererText()
+        column_slot = Gtk.TreeViewColumn("Slot", renderer_slot, text=0)
+        treeview.append_column(column_slot)
+        renderer_value = Gtk.CellRendererText()
+        column_value = Gtk.TreeViewColumn("Value", renderer_value, text=1)
+        treeview.append_column(column_value)
+        self.packet_view = treeview
+
+        scrolledwindow.add(treeview)
+        vbox_right.pack_start(scrolledwindow, True, True, 0)
 
         label = Gtk.Label("Menu")
-        vbox_left.pack_start(label, True, True, 0)
+        vbox_left.pack_start(label, False, True, 0)
 
         button = Gtk.Button(label="Reset Count")
         button.connect("clicked", self.btn_reset_count)
         label.set_mnemonic_widget(button)
-        vbox_left.pack_start(button, True, True, 0)
+        vbox_left.pack_start(button, False, True, 0)
 
         button = Gtk.Button(label="Single Capture")
         button.connect("clicked", self.btn_single_capture)
         label.set_mnemonic_widget(button)
-        vbox_left.pack_start(button, True, True, 0)
+        vbox_left.pack_start(button, False, True, 0)
 
         button = Gtk.Button(label="Continuous Capture")
         button.connect("clicked", self.btn_multi_capture)
         label.set_mnemonic_widget(button)
-        vbox_left.pack_start(button, True, True, 0)
+        vbox_left.pack_start(button, False, True, 0)
+
+        channel_monitor_frame = Gtk.Frame(label="Channel Monitor")
+
+        channel_monitor_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        channel_monitor_box.set_border_width(10)
+
+        channel_sizes_store = Gtk.ListStore(int, str)
+        channel_sizes_store.append([3, "3 Slot Lighting Channel"])
+        channel_sizes_store.append([7, "7 Slot Lighting Channel"])
+        channel_sizes_store.append([8, "8 Slot Lighting Channel"])
+
+        channel_sizes_combo = Gtk.ComboBox.new_with_model_and_entry(channel_sizes_store)
+        self.channel_sizes_combo = channel_sizes_combo
+        #name_combo.connect("changed", self.on_name_combo_changed)
+        channel_sizes_combo.set_entry_text_column(1)
+        channel_monitor_box.pack_start(channel_sizes_combo, False, False, 0)
 
         button = Gtk.Button(label="New Channel Monitor")
         button.connect("clicked", self.btn_create_channel_monitor)
         label.set_mnemonic_widget(button)
-        vbox_left.pack_start(button, True, True, 0)
+        channel_monitor_box.pack_start(button, False, True, 0)
+
+        channel_monitor_frame.add(channel_monitor_box)
+        vbox_left.pack_start(channel_monitor_frame, False, True, 0)
 
         self.add(hbox)
 
@@ -74,10 +107,14 @@ class DisplayUI(Gtk.Window):
     def on_timeout(self, event=None):
         self.packet_count_label.set_text("Packet Count: " + str(self.packet_count_value.value))
         if self.mode == "SINGLE_CAPTURE":
-            self.packet_data.get_buffer().set_text(" ".join([hex(x).ljust(4, "0") for x in self.packet_last_value[:]]))
+            index = 0
+            for value in self.packet_last_value[:]:
+                self.packet_data[index] = [str(index + 1), hex(value).ljust(4, "0")]
             self.mode = "WAITING"
         if self.mode == "MULTI_CAPTURE":
-            self.packet_data.get_buffer().set_text(" ".join([hex(x).ljust(4, "0") for x in self.packet_last_value[:]]))
+            index = 0
+            for value in self.packet_last_value[:]:
+                self.packet_data[index] = [str(index + 1), hex(value).ljust(4, "0")]
         return True
 
     def btn_reset_count(self, event):
@@ -90,10 +127,27 @@ class DisplayUI(Gtk.Window):
         self.mode = "MULTI_CAPTURE"
 
     def btn_create_channel_monitor(self, event):
-        print("IN")
-        self.channel_windows.append(UIChannelDisplay(self))
-        print(self.channel_windows)
-        print("OUT")
+        channel_size = 3
+        start_slot = 1
+        active = self.packet_view.get_selection().get_selected()[1]
+        if active is not None:
+            start_slot = int(self.packet_view.get_model()[active][0])
+        active = self.channel_sizes_combo.get_active_iter()
+        if active is not None:
+            model = self.channel_sizes_combo.get_model()
+            row_id, name = model[active][:2]
+            channel_size = row_id
+        elif self.channel_sizes_combo.get_child().get_text() != "":
+            entry_text = self.channel_sizes_combo.get_child().get_text()
+            entry_value = 0
+            try:
+                channel_size = int(entry_text)
+            except:
+                channel_size = 3
+        if not 513 > channel_size > 0:
+            channel_size = 3
+        new_window = UIChannelDisplay(self, start_slot, channel_size)
+        self.channel_windows[new_window.id] = new_window
 
     def on_quit(self, event=None, event2=None):
         self.ui_running.value = 0
