ef457beb6150613f49a6216b5f34ab7da83eac83
Jacob Allen <jacob@polyomino.xyz>
2018-03-06 11:48:43 +0000

    Added help to display

--- Testing/Display/ui_channel_display.py
+++ Testing/Display/ui_channel_display.py
@@ -28,7 +28,10 @@ class UIChannelDisplay(Gtk.Window):
         liststore = Gtk.ListStore(str, str)
         self.packet_data = liststore
         for i in range(channel_size):
-            liststore.append([str(i+start_slot), "0x00"])
+            if i+start_slot < 513:
+                liststore.append([str(i+start_slot), "0x00"])
+            else:
+                liststore.append([str(i+start_slot), "Out of Range"])
         self.update_values()
 
         treeview = Gtk.TreeView(model=liststore)
@@ -48,21 +51,8 @@ class UIChannelDisplay(Gtk.Window):
             label = Gtk.Label(label="Channel Colour")
             vbox.pack_start(label, False, True, 0)
 
-            hex_code = "ffffff"
-            if channel_size == 3:
-                values = self.parent.packet_last_value[self.start_slot-1:self.start_slot + 2]
-                rgb = [hex(value)[2:].rjust(2, "0") for value in values]
-                hex_code = "".join(rgb)
-            if channel_size > 3:
-                values = self.parent.packet_last_value[self.start_slot-1:self.start_slot + 3]
-                rgb = [hex(int(float(value)*(float(values[0])/255.0)))[2:].rjust(2, "0") for value in values[1:4]]
-                hex_code = "".join(rgb)
-            color = Gdk.color_parse("#" + hex_code)
-            rgba = Gdk.RGBA.from_color(color)
-
             area = Gtk.DrawingArea()
             area.set_size_request(24, 24)
-            area.override_background_color(0, rgba)
 
             self.colour_area = area
             vbox.pack_start(area, False, True, 0)
@@ -70,9 +60,11 @@ class UIChannelDisplay(Gtk.Window):
             label = Gtk.Label(label="Channel Colour Hex Code")
             vbox.pack_start(label, False, True, 0)
 
-            label = Gtk.Label(label="#" + hex_code)
+            label = Gtk.Label(label="#ffffff")
             self.colour_label = label
             vbox.pack_start(label, False, True, 0)
+
+            self.update_values()
 
         self.add(vbox)
 
@@ -92,17 +84,17 @@ class UIChannelDisplay(Gtk.Window):
             if self.channel_size == 3:
                 values = self.parent.packet_snapshot[self.start_slot-1:self.start_slot + 2]
                 rgb = [hex(value)[2:].rjust(2, "0") for value in values]
-                hex_code = "".join(rgb)
+                hex_code = "".join(rgb).ljust(6, "0")
             if self.channel_size > 3:
                 values = self.parent.packet_snapshot[self.start_slot-1:self.start_slot + 3]
                 rgb = [hex(int(float(value)*(float(values[0])/255.0)))[2:].rjust(2, "0") for value in values[1:4]]
-                hex_code = "".join(rgb)
+                hex_code = "".join(rgb).ljust(6, "0")
             color = Gdk.color_parse("#" + hex_code)
             rgba = Gdk.RGBA.from_color(color)
 
             self.colour_area.override_background_color(0, rgba)
 
-            self.colour_label = Gtk.Label(label="#" + hex_code)
+            self.colour_label.set_text("#" + hex_code)
         return True
 
     def on_close(self, event):
