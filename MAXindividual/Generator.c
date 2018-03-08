34a0f116747fc5b99d5bf8e2b197fe1fcbd515d1
M Franchot <mf977@pc774s.cs.york.ac.uk>
2018-03-08 15:27:16 +0000

    working parameter input for MAXindividual through command line

---
+++ MAXindividual/Generator.c
@@ -0,0 +1,409 @@
+#include "GenericLibraries.c"
+
+#define setdata(int1,int2,int3) data[0] = int1; data[1] = int2; data[2] = int3
+#define _cb(no,com) case no: {com;break;}
+#define menu(com1, com2, com3, com4, com5, \
+  com6, com7, com8, com9, com10, com11,\
+  com12, com13, com14, com15, com16) \
+  do { uint8_t key = read_keypress(); \
+  switch (key){\
+    _cb(0, com1)  _cb(1, com2)  _cb(2, com3)  _cb(3, com4)\
+    _cb(4, com5)  _cb(5, com6)  _cb(6, com7)  _cb(7, com8)\
+    _cb(8, com9)  _cb(9, com10) _cb(10,com11) _cb(11,com12)\
+    _cb(12,com13) _cb(13,com14) _cb(14,com15) _cb(15,com16)}\
+  } while(0)
+#define clear_number_input() number_input[0] = 0;number_input[1] = 0;number_input[2] = 0; in_count = 0
+#define DELAY 500000
+
+int8_t char_buff[1];
+int8_t out_buff[32];
+uint8_t colour[10][3];
+uint8_t sequence[4][16];
+uint8_t sequence_lengths[4] = {4,0,0,0};
+uint8_t sequence_repeat[4] = {0,0,0,0};
+static uint8_t LCDcount = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Extra Functions
+
+uint8_t input_translate(uint8_t number_input[3], uint8_t in_count);
+void set_default(void);
+
+// Non-state functions
+void display_colour(uint8_t number);
+void display_sequence(uint8_t number); // printKeyToLCD(number,LCDcount);
+void copy_sequence(uint8_t def_seq_number, uint8_t seq_tobecopied_number, uint8_t* in_count);
+void add_seq_to_display(uint8_t number, uint8_t in_count);
+void clear_sequence(uint8_t number);
+
+// States
+void define_colour(uint8_t number);
+void define_sequence(uint8_t number);
+void main_menu(void);
+void def_menu(void);
+void opt_menu(void);
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//maths and extra stuff
+uint8_t input_translate(uint8_t number_input[3], uint8_t in_count){
+  uint16_t output = 0;
+  if(in_count == 0){
+    output = 0;
+  } else if (in_count == 1){
+    output = number_input[0];
+  } else if (in_count == 2){
+    output = (number_input[0] * 10) + number_input[1];
+  } else if (in_count == 3){
+    output = (number_input[0] * 100) + (number_input[1] * 10) + number_input[2];
+  }
+  if (output > 255) output = 255;
+
+  return (uint8_t) output;
+}
+
+// Non-state functions
+
+void set_default(void){
+  int i;
+  int j;
+  //default is 0
+  for (i = 0; i < 10; i++){
+    colour[i][0] = 0;
+    colour[i][1] = 0;
+    colour[i][2] = 0;
+  }
+  //set colours:
+  for (i = 0; i < 3; i++){
+    //1:Red, 2:Green, 3:Blue
+    colour[i+1][i] = 255;
+    //1:Yellow, 2:Cyan, 3:Magenta
+    colour[i+4][i] = 255;
+    colour[i+4][(i+1)%3] = 255;
+    //Greys and whites
+    for (j = 0; j < 3; j++){
+      colour[i+7][j] = (i+1)*85;
+    }
+    //0 is left black
+  }
+  for (i = 0; i < 4; i++){
+    sequence_lengths[i] = 4;
+    sequence[0][i] = (i%3)+1; //A: Red-Green-Blue-Red
+    sequence[1][i] = (i%3)+4; //B: Yellow-Cyan-Purple-Yellow
+    sequence[2][(i+1)%4] = (i+7)%10; //C: Fade-in
+    sequence[3][i] = rand() % 10; //D: Random
+    sequence[3][i+4] = rand() % 10;
+    sequence[3][i+8] = rand() % 10;
+    sequence[3][i+12] = rand() % 10;
+  }
+  sequence_lengths[3] = 16;
+}
+
+void display_colour(uint8_t number){
+  int8_t str[1];
+  display_LCD("Disp. Colour:   ",0);
+  sprintf(str,"%d",number);
+  display_LCD(str, 14);
+  //set data
+  setdata(colour[number][0],colour[number][1],colour[number][2]);
+  //light up lamp
+  send_data_UART(BLOCKING);
+
+}
+void display_sequence(uint8_t number){
+  display_LCD("Disp. Seq:      ",0);
+  uint8_t letter[1] = {number+65};
+  display_LCD(letter, 10);
+  //display sequence
+  int i = 0;
+  int rep;
+  for (rep = 0; rep <= sequence_repeat[number]; rep++){
+    for(i = 0; i < sequence_lengths[number]; i++){
+      if (sequence[number]){
+        setdata(colour[sequence[number][i]][0],colour[sequence[number][i]][1],colour[sequence[number][i]][2]);
+      }
+      send_data_UART(BLOCKING);
+      Delay(DELAY);
+    }
+  }
+}
+void display_all(){
+  display_LCD("Displaying all: -              -",0);
+  int8_t str[1];
+  //Colours
+  int i;
+  for (i = 0; i < 10; i++){
+    sprintf(str, "%d",i);
+    display_LCD(str,17+i);
+    display_colour(i);
+    Delay(500000);
+  }
+  display_LCD("A",17+(i++));
+  display_sequence(0);
+  Delay(500000);
+  display_LCD("B",17+(i++));
+  display_sequence(1);
+  Delay(500000);
+  display_LCD("C",17+(i++));
+  display_sequence(2);
+  Delay(500000);
+  display_LCD("D",17+(i++));
+  display_sequence(3);
+  Delay(500000);
+}
+void copy_sequence(uint8_t def_seq_number, uint8_t seq_tobecopied_number, uint8_t* in_count){
+  uint8_t copy_seq[16];
+  int i;
+  //make a copy of copy_seq because it may be pointing to the same as def_seq.
+  for (i = 0; i < sequence_lengths[seq_tobecopied_number]; i++){copy_seq[i] = sequence[seq_tobecopied_number][i];}
+
+  for (i = 0; i < sequence_lengths[seq_tobecopied_number]; i++){
+    sequence[def_seq_number][*in_count] = copy_seq[i];
+    ++*in_count;
+  }
+}
+void add_seq_to_display(uint8_t number, uint8_t in_count){
+  int8_t str[1];
+  int i;
+
+  for (i = 0; i < sequence_lengths[number]; i++){
+    sprintf(str, "%d", sequence[number][i]+1);
+    display_LCD(str, 16+in_count+i);
+  }
+}
+void clear_sequence(uint8_t number){
+  sequence_lengths[number] = 0;
+}
+
+// States
+void define_colour(uint8_t number){
+  int8_t string[32];
+  display_LCD("Set rgb ->A,B,C,Save to mem: ->D",0);
+  Delay(1000000);
+  sprintf(string,"Col %d: VAL: --- SAVE rgb ->A,B,C", (number+1));
+  display_LCD(string,0);
+
+  uint8_t number_input[3];
+  number_input[0] = 0;
+  number_input[1] = 0;
+  number_input[2] = 0;
+  uint8_t r = 0;
+  uint8_t g = 0;
+  uint8_t b = 0;
+  uint8_t r_str[3];
+  uint8_t g_str[3];
+  uint8_t b_str[3];
+  uint8_t in_count = 0;
+  uint8_t reset = 0;
+
+  while(1){
+    if (reset==1){
+      sprintf(string,"Col %d: VAL: --- SAVE rgb ->A,B,C", number);
+      display_LCD(string,0);
+      clear_number_input();
+      reset = 0;
+    }
+
+    menu(
+      if(in_count < 3) display_LCD("1",12+in_count); number_input[in_count] = 1; in_count++,
+      if(in_count < 3) display_LCD("2",12+in_count); number_input[in_count] = 2; in_count++,
+      if(in_count < 3) display_LCD("3",12+in_count); number_input[in_count] = 3; in_count++,
+      r = input_translate(number_input, in_count);
+        display_LCD("Saved RED val.  Save to mem: ->D",0);
+        Delay(1000000);
+        reset = 1,
+      if(in_count < 3) display_LCD("4",12+in_count); number_input[in_count] = 4; in_count++,
+      if(in_count < 3) display_LCD("5",12+in_count); number_input[in_count] = 5; in_count++,
+      if(in_count < 3) display_LCD("6",12+in_count); number_input[in_count] = 6; in_count++,
+      g = input_translate(number_input, in_count);
+        display_LCD("Saved GREEN val.Save to mem: ->D",0);
+        Delay(1000000);
+        reset = 1,
+      if(in_count < 3) display_LCD("7",12+in_count); number_input[in_count] = 7; in_count++,
+      if(in_count < 3) display_LCD("8",12+in_count); number_input[in_count] = 8; in_count++,
+      if(in_count < 3) display_LCD("9",12+in_count); number_input[in_count] = 9; in_count++,
+      b = input_translate(number_input, in_count);
+        display_LCD("Saved BLUE val. Save to mem: ->D",0);
+        Delay(1000000);
+        reset = 1,
+      display_LCD("---",12);
+        clear_number_input(),
+      if(in_count < 3) display_LCD("0",12+in_count); number_input[2-in_count] = 0; in_count++,
+      return,
+      colour[number][0] = r;
+        colour[number][1] = g;
+        colour[number][2] = b;
+        display_LCD("Saved to memory.R:   G:   B:    ",0);
+        sprintf(r_str, "%d",r);
+        sprintf(g_str, "%d",g);
+        sprintf(b_str, "%d",b);
+        display_LCD(r_str, 18);
+        display_LCD(g_str, 23);
+        display_LCD(b_str, 28);
+        display_colour(number);
+        Delay(1000000);
+        reset = 1;
+        return
+    );
+  }
+}
+void define_sequence(uint8_t number){
+  display_LCD("Type Sequence.   SAVE: * CLR: #",0);
+  Delay(1000000);
+  //uint8_t str[32];
+  int8_t letter[1];
+  switch (number) {
+    case 0: letter[0]='A'; break;
+    case 1: letter[0]='B'; break;
+    case 2: letter[0]='C'; break;
+    case 3: letter[0]='D'; break;
+  }
+
+  uint8_t reset = 1;
+  uint8_t cancel = 0;
+  uint8_t in_count = 0;
+  while(1){
+    if (reset == 1){
+      display_LCD("Seq:  SAV*/CLR#",0);
+      display_LCD(letter, 4);
+      display_LCD("                ",16);
+      in_count = 0;
+      reset = 0;
+    }
+    if (cancel==1 && in_count > 0){
+      cancel = 0;
+      display_LCD("CLR#",11);
+    }
+    menu(
+      if (in_count < 16) sequence[number][in_count] = 1; display_LCD("1",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 2; display_LCD("2",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 3; display_LCD("3",16+in_count); in_count++,
+      add_seq_to_display(0,in_count); copy_sequence(number,0,&in_count),
+      if (in_count < 16) sequence[number][in_count] = 4; display_LCD("4",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 5; display_LCD("5",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 6; display_LCD("6",16+in_count); in_count++,
+      add_seq_to_display(1,in_count); copy_sequence(number,1,&in_count),//B
+      if (in_count < 16) sequence[number][in_count] = 7; display_LCD("7",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 8; display_LCD("8",16+in_count); in_count++,
+      if (in_count < 16) sequence[number][in_count] = 9; display_LCD("9",16+in_count); in_count++,
+      add_seq_to_display(2,in_count); copy_sequence(number,2,&in_count),//C
+      display_LCD("Saved Seq  .   ",0);display_LCD(letter, 10);Delay(1000000);return,//*
+      if (in_count < 16) sequence[number][in_count] = 0; display_LCD("0",16+in_count); in_count++,
+      if (cancel==0){
+          display_LCD("CXL#    = = CLR = =  ",11);
+          clear_sequence(number);
+          cancel=1;
+          in_count = 0;
+          Delay(1000000);
+          display_LCD("                ",16);
+        } else {
+          display_LCD(" = = CANCEL = = ", 16);
+          Delay(1000000);
+          return;
+        },//#
+      add_seq_to_display(3,in_count); copy_sequence(number,3,&in_count)//D
+    );
+    sequence_lengths[number] = in_count;
+  }
+}
+
+void def_menu(void){
+  while(1) {
+    display_LCD("DEF: 0-9 or A-D *:DISP ALL #:CXL",0);
+    menu(
+      define_colour(1),
+      define_colour(2),
+      define_colour(3),
+      define_sequence(0),
+      define_colour(4),
+      define_colour(5),
+      define_colour(6),
+      define_sequence(1),
+      define_colour(7),
+      define_colour(8),
+      define_colour(9),
+      define_sequence(2),
+      display_all(),
+      define_colour(0),
+      return,
+      define_sequence(3)
+    );
+  }
+}
+void opt_menu(void){
+  display_LCD("Select Seq and  times to repeat.",0);
+  Delay(1000000);
+  display_LCD("SEQ: A RPT: 0   SAVE: * CXL: #  ",0);
+
+  uint8_t repeat = 0;
+  uint8_t sequence = 0;
+  while(1){
+    menu(
+      repeat = 1; display_LCD("1", 12),
+      repeat = 2; display_LCD("2", 12),
+      repeat = 3; display_LCD("3", 12),
+      sequence = 0; display_LCD("A", 5),
+      repeat = 4; display_LCD("4", 12),
+      repeat = 5; display_LCD("5", 12),
+      repeat = 6; display_LCD("6", 12),
+      sequence = 1; display_LCD("B", 5),
+      repeat = 7; display_LCD("7", 12),
+      repeat = 8; display_LCD("8", 12),
+      repeat = 9; display_LCD("9", 12),
+      sequence = 2; display_LCD("C", 5),
+      sequence_repeat[sequence] = repeat; display_LCD("  Saved repeat.                 ",0); Delay(1000000); return,
+      repeat = 0; display_LCD("0", 12),
+      return,
+      sequence = 3; display_LCD("D", 5)
+    );
+  }
+}
+void main_menu(void){
+  uint8_t reset = 1;
+  while (1) {
+    if (reset==1) display_LCD("0-9:Col, A-D:Seq*: Def, #:Repeat", 0); reset = 0;
+    menu(
+      display_colour(1),
+      display_colour(2),
+      display_colour(3),
+      display_sequence(0); reset = 1,
+      display_colour(4),
+      display_colour(5),
+      display_colour(6),
+      display_sequence(1); reset = 1,
+      display_colour(7),
+      display_colour(8),
+      display_colour(9),
+      display_sequence(2); reset = 1,
+      def_menu(); reset = 1,
+      display_colour(0),
+      opt_menu(); reset = 1,
+      display_sequence(3); reset = 1
+    );
+  }
+}
+/*
+int main(void){
+  Full_Init();
+  display_LCD("                                ",0);
+
+  set_default();
+
+  /* //DEBUG light
+  set_basic_data();
+  display_colour(1);
+  send_data_UART(BLOCKING);
+  //
+  /* //DEBUG LCD and keypad
+  LCD_clear();
+  while(1){
+    get_keypad_press(read_buff);
+    display_colour(0);
+    printKeyToLCD(decode_keypad(read_buff[0]),LCDcount);
+  }
+  //
+
+  while(1) main_menu();
+}
+*/
--- MAXindividual/UART_mbed.c
+++ MAXindividual/UART_mbed.c
@@ -5,7 +5,24 @@
 //#include "GenericLibraries.c"
 #include "lib/empr_lib_serial.c"
 #include "lpc17xx_uart.h"
+#include "GenericLibraries.c"
 #include <stdio.h>
+
+uint8_t command_parameters[8];
+
+//Settings
+enum eTransitionMode{Switch,Black,Fade,FadeBlack};
+
+//Data
+uint8_t colour[10][3];
+struct Sequence {
+  uint8_t colours[16];
+  uint8_t repeat;
+  uint8_t transition_mode;
+  uint8_t transition_speed;
+  uint8_t hold_time;
+};
+struct Sequence sequence[16];
 
 
 void clear_array(uint8_t array[INPUTSIZE]){
@@ -22,7 +39,10 @@ uint8_t compare_command(uint8_t input[INPUTSIZE], uint8_t command[INPUTSIZE]){
   EL_SERIAL_Print(NEXTLINE);
   EL_SERIAL_Print(command);*/
   for (i = 0; i < INPUTSIZE; i++){
-    if ((input[i] != 0) && (input[i] != command[i])){
+    //for special to-be-saved input
+    if (command[i] == '/') {
+      return 1;
+    } else if ((input[i] != 0) && (input[i] != command[i])){
       //DEBUG uint8_t output[32];
       //DEBUG sprintf(output, "\"%c\" is different from \"%c\"", input[i], command[i]);
       //DEBUG EL_SERIAL_Print(output);
@@ -33,19 +53,56 @@ uint8_t compare_command(uint8_t input[INPUTSIZE], uint8_t command[INPUTSIZE]){
   return 1;
 }
 
+uint8_t get_cmd_input(uint8_t input[INPUTSIZE], uint8_t params){
+  uint8_t i;
+  uint8_t input_count = 0;
+  for (i = 0; i < INPUTSIZE; i++){
+    uint8_t param = 0;
+    //continues scrolling through array incrementing i, but registering the parameters.
+    if (input[i] == ' '){
+      while ((input[i+1] != ' ') && (input[i+1])){
+        i++;
+        //translate char to int: (x - 128), and add to number
+        param = (param * 10) + (input[i]-48);
+        uint8_t str[16];
+        sprintf(str, "\n\r\t%d\n\t",input[i]);
+        EL_SERIAL_Print(str);
+      }
+      command_parameters[input_count] = param;
+      //So for every space, aka parameter, the input_count is incremented.
+      input_count++;
+    }
+  }
+  return (input_count == params);
+}
+
 void command(uint8_t input[INPUTSIZE]){
   //"switch" on the full command:
-
+  uint8_t output[32];
   //DEBUG uint8_t output[32];
   //DEBUG sprintf(output, "\n\rCommand registered: %d", compare_command(input,"test"));
   //DEBUG EL_SERIAL_Print(output);
 
-  if (compare_command(input,"test")){
+  if (compare_command(input,"test/")){
     EL_SERIAL_Print("\n\rTesting Successful!\n\r");
     clear_array(input);
-  } else if (compare_command(input, "help")){
-    EL_SERIAL_Print("\n\n\rPossible commands: \n\r\t-help\n\r\t-repeat\n\r\t-display\n\r\t-transition\n\r");
-  } else {
+  } else if (compare_command(input, "help/")){
+    EL_SERIAL_Print("\n\n\rPossible commands: \n\r\t-help\
+    \n\r\t-repeat s r   :  s->sequence number, r->times to repeat\
+    \n\r\t-displayseq s :  s->sequence number\
+    \n\r\t-displaycol c :  c->colour number\
+    \n\r\t-trans s x    :  s->sequence number, \n\r    x:{0-\"Switch\", 1-\"Black\", 2-\"Fade\", 3-\"Fade through black\"} \n\r");
+  } else if (compare_command(input, "repeat/")){
+    if(get_cmd_input(input,2)){
+      sequence[command_parameters[0]].repeat = command_parameters[1];
+      sprintf(output, "\n\rSequence %d set to play %d times.\n\r",
+        command_parameters[0],command_parameters[1]);
+      EL_SERIAL_Print(output);
+    } else {
+      EL_SERIAL_Print("\n\rWrong parameter inputs. [repeat s r   :  s->sequence number, r->times to repeat]\n\r");
+    }
+  }
+  else {
     EL_SERIAL_Print("\n\rCommand not recognized. Type \"help\" for a list of commands.\n\r");
   }
 
@@ -53,8 +110,27 @@ void command(uint8_t input[INPUTSIZE]){
 
 }
 
+void display_light(struct Sequence* sequence){
+  int i;
+  for (i = 0; i < sequence->repeat; i++){
+    //send_data_UART(BLOCKING);
+    if (sequence->transition_mode == Switch) {
+      //delay
+      //display next colour.
+    } else if (sequence->transition_mode == Black) {
+      //turn off
+      //delay
+      //display next colour
+    } else if (sequence->transition_mode == Fade) {
+      //using delay, loop and slowly go from one colour to another
+    } else if (sequence->transition_mode == FadeBlack) {
+      //using delay, loop and slowly go to black
+      //then, slowly go to next colour
+    }
+  }
+}
 
-int main() {
+int main(void) {
   EL_SERIAL_Init();
   EL_SERIAL_Print("\n");
   EL_SERIAL_Print("USB test code\n\r");
@@ -63,6 +139,7 @@ int main() {
   uint8_t inc = 0;
   uint8_t byte[2];
   byte[1] = 0;
+
 
 
   //Main loop: takes in command line input and modifies everything accordingly.
