a20c3547c747401ef0743f3d442a982e6d5685fc
Jacob Allen <jacob@polyomino.xyz>
2018-03-12 14:33:20 +0000

    Cleaned up display directory

---
+++ Display/display.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from monitor_interface import MonitorInterface
+from user_interface import DisplayUI
+from threading import Thread, Semaphore
+from multiprocessing import Process, Array, Value
+
+class Display(object):
+    def __init__(self):
+        self.ui = DisplayUI()
+        self.ui_thread = Process(target=lambda: DisplayUI.create_ui(self.ui))
+        self.monitor = MonitorInterface()
+        self.monitor_thread = Thread(target=self.monitor.run)
+        self.main_thread = Thread(target=self.main_loop)
+        self.packet_count = 0
+        self.running = False
+
+    def run(self):
+        self.ui_thread.start()
+        self.monitor_thread.start()
+        self.running = True
+        self.main_thread.start()
+
+    def stop(self, ui=None, event=None):
+        self.monitor.stop()
+        self.running = False
+
+    def main_loop(self):
+        while(self.running):
+            if self.ui.ui_running.value == 0:
+                self.stop()
+                return
+            if self.ui.reset_count.value == 1:
+                self.packet_count = 0
+                self.ui.reset_count.value = 0
+            if not self.running:
+                return
+            if(self.monitor.buffer_semaphore.acquire(True, 2.0)):
+                if self.monitor.packets:
+                    packet = self.monitor.packets.pop()
+                    self.packet_count += 1
+                    self.ui.packet_count_value.value = self.packet_count
+                    self.ui.packet_last_value = Array("B", packet)
+
+if __name__ == "__main__":
+    disp = Display()
+    disp.run()
---
+++ Display/monitor_interface.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+from threading import Semaphore
+import sys, os
+import serial
+
+class MonitorInterface(object):
+    inst_index = 0
+
+    def __init__(self, tty_location="/dev/ttyACM0"):
+        try:
+            self.tty_handle = serial.Serial(tty_location, baudrate=230400, parity=serial.PARITY_NONE)
+            #self.tty_handle = os.open(tty_location, os.O_RDWR)
+        except IOError:
+            raise Exception("Unable to open TTY")
+        self.buffer = ""
+        self.packets = []
+        self.running = False
+        self.buffer_semaphore = Semaphore(0)
+        self.inst_ind = self.inst_index
+        self.inst_index += 1
+
+    def run(self):
+        self.running = True
+        while self.running:
+            #self.buffer += os.read(self.tty_handle, 16).decode("ascii").replace("\n","").replace("\r","").replace("\0","")
+            self.buffer += self.tty_handle.readline().decode("ascii", "ignore").replace("\n","").replace("\r","").replace("\0","")
+
+            exclamation_index = self.buffer.index("!") if "!" in self.buffer else -1
+            if(exclamation_index > 0):
+                self.buffer = self.buffer[exclamation_index:]
+
+            exclamation_count = self.buffer.count("!")
+            while(exclamation_count > 1):
+                second_exclamation_index = self.buffer[1:].index("!") + 1
+                packet_data = self.buffer[1:second_exclamation_index][:1024]
+                self.buffer = self.buffer[second_exclamation_index:]
+                last_value = 0
+                packet_len = len(packet_data)
+                packet = []
+                read_count = 0
+                success = True
+                while packet_len > 0:
+                    read_count += 1
+                    if read_count > 64:
+                        success = False
+                        break
+                    if packet_data[0] == "X":
+                        try:
+                            run_length = int(packet_data[1:3], 16)
+                        except Exception:
+                            exclamation_count = self.buffer.count("!")
+                            continue
+                        packet_data = packet_data[3:]
+                        packet += [last_value]*run_length
+                        packet_len -= 3
+                    else:
+                        try:
+                            slot_value = int(packet_data[:2], 16)
+                        except Exception:
+                            exclamation_count = self.buffer.count("!")
+                            continue
+                        packet.append(slot_value)
+                        last_value = slot_value
+                        packet_data = packet_data[2:]
+                        packet_len -= 2
+                if len(packet) != 512:
+                    exclamation_count = self.buffer.count("!")
+                    continue
+                if success:
+                    self.packets.append(packet)
+                    self.buffer_semaphore.release()
+                exclamation_count = self.buffer.count("!")
+
+        self.buffer = ""
+
+    def stop(self):
+        self.running = False
+        self.buffer_semaphore.release()
+
+    def clean(self):
+        self.buffer = ""
+        self.packets = []
+
+if __name__ == "__main__":
+    print("This module will not function properly if run, please run the core display file instead.")
+    monitor_test = MonitorInterface()
+    monitor_test.run()
